8bdb44e503230ac34a80cd70b9061917069d7532
GabrielCPM <gabrielcpcm@al.insper.edu.br>
2025-04-28 11:24:46 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,14 @@ def rolar_dados (dados):
         lista_valores.append(randint(1,6))
         i += 1
     return lista_valores
+
+def guardar_dado (lista_rodados, lista_guardados, indice):
+    lista_ex2 = []
+    lista_guardados.append(lista_rodados[indice])
+    del lista_rodados[indice]
+    lista_ex2.append(lista_rodados)
+    lista_ex2.append(lista_guardados)
+
+    return lista_ex2
+
+    
