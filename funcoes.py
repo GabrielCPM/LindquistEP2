585175cead20df1fcf57b75aa316ff75e7011e48
Lucasfl3 <lucasfl3@al.insper.edu.br>
2025-04-28 11:25:37 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,14 @@ def rolar_dados (dados):
         lista_valores.append(randint(1,6))
         i += 1
     return lista_valores
+
+
+def guardar_dado (dados_rolados, dados_guardados, indice):
+    dados_rodados= []
+    for i in range (dados_rolados):
+        if i == indice:
+            dados_guardados.append(dados_rolados[i])
+        else:
+            dados_rodados.append(dados_rolados[i])
+    lista_final = (dados_rolados, dados_guardados)
+    return lista_final
